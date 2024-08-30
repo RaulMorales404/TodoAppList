fb679ff643ea529edbc07ad94d0c2ef27e74e7c2
RaulMorales404 <r20160106@gmail.com>
2024-08-30 04:01:45 -0600

    add config firebase

---
+++ TodoApp/Services/CreateTask/firebaseInterop.js
@@ -0,0 +1,11 @@
+﻿window.firebaseInterop = {
+    uploadFile: function (path, file) {
+        return storage.ref(path).put(file).then(snapshot => snapshot.ref.getDownloadURL());
+    },
+    getFileUrl: function (path) {
+        return storage.ref(path).getDownloadURL();
+    },
+    addDocument: function (collectionName, document) {
+        return db.collection(collectionName).add(document);
+    }
+};
